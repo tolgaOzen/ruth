9402f5a37341211b2c857117dff2435e312fb6f6
Tolga Özen <mtolgaozen@gmail.com>
2020-01-13 02:59:02 +0300

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # ruth
 Use of try, catch, finally in Golang using the same syntax as java / python / php
+
+<p align="center">
+<img src="https://user-images.githubusercontent.com/39353278/72227565-313f4380-35af-11ea-8fc6-c33a5945b867.png" alt="ruth" width="300">
+</p>
+
+
+
---
+++ ruth.go
@@ -0,0 +1,68 @@
+package ruth
+
+
+type Exception struct {
+	Error interface{}
+}
+
+type Section struct {
+	Exception
+	innerTry     func()
+}
+
+func Try(innerTry func()) (section Section) {
+
+	section = Section{innerTry: innerTry}
+
+	defer func() {
+		exception := Exception{}
+		exception.Error = recover()
+		section.Exception = exception
+	}()
+
+	section.innerTry()
+
+	return
+
+}
+
+func (s Section) Catch(innerCatch func(e Exception)) (section Section) {
+
+	if s.Exception.Error != nil {
+
+		defer func() {
+
+			if err := recover(); err != nil {
+				s.Exception.Error = err
+			} else {
+				s.Exception.Error = nil
+			}
+
+			section = s
+		}()
+
+		innerCatch(s.Exception)
+		return s
+	}
+
+	return
+}
+
+
+func (s Section) TryAgain() (section Section) {
+
+	defer func() {
+		if err := recover(); err != nil {
+			s.Exception.Error = err
+			section = s
+		}
+	}()
+
+	s.innerTry()
+	return
+}
+
+
+func (s Section) Finally(innerFinally func()) {
+	innerFinally()
+}
